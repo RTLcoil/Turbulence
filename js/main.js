425990e30d9e6d7fef8ba5ada8dd572fd6935c98
RTLcoil <oleg@rtl.co.il>
2015-05-06 10:20:31 +0300

    actual files

--- js/main.js
+++ js/main.js
@@ -76,7 +76,7 @@ var tmpl = {
 
 	popupIconTitle: '<div class="i-item__popup" style="box-shadow: 0 0 0 4px {color}"><div class="i-item__popup-frame"></div><a href="{url}" class="i-item__popup-img"><img src="{img}" alt="" class="i-item__popup-work"></a><div class="i-item__popup-title" style="color: {color}">{title}<span style="color: {color}">{author}</span></div><div class="i-item__popup-tags" style="color: {color}">{tags}</div></div>',
 	
-	popupArtist: '<div class="i-item__popup i-item__popup_person" style="box-shadow: 0 0 0 4px {color}; color: {color}"><div class="i-item__popup_map"><iframe src="{map}" width="100%" height="100%" frameborder="0" style="border:0"></iframe></div><img src="{artist}" alt="" class="i-item__popup-artist"><a href="{artistUrl}" class="i-item__popup-name" style="color: {color}">{name}</a><div class="i-item__popup-place" style="color: {color}">{place}</div><div class="i-item__popup-works">{works}</div></div>'
+	popupArtist: '<div class="i-item__popup i-item__popup_person" style="box-shadow: 0 0 0 4px {color}; color: {color}"><div class="i-item__popup_map"><div class="acf-map"><div class="marker" data-lat="{mapLat}" data-lng="{mapLng}"></div></div></div><img src="{artist}" alt="" class="i-item__popup-artist"><a href="{artistUrl}" class="i-item__popup-name" style="color: {color}">{name}</a><div class="i-item__popup-place" style="color: {color}">{place}</div><div class="i-item__popup-works">{works}</div></div>'
 };
 
 (function($) {
@@ -550,8 +550,10 @@ var tmpl = {
 						.replace('{artist}', data.artist)
 						.replace('{place}', data.place)
 						.replace('{name}', name[0])
-						.replace('{artistUrl}', name[1]);
-
+						.replace('{artistUrl}', name[1])
+						.replace('{mapLng}', data.mapLng)
+						.replace('{mapLat}', data.mapLat);
+					
 					if("customColor" in data && data.customColor) {
 						var re = new RegExp("{color}", "gi");
 						while(re.test(html)) {
@@ -694,6 +696,11 @@ var tmpl = {
 							} else {
 								$(".i-item__popup", $closest).css("left", -75);
 							}
+							
+							if($(".marker", $closest).length && !$(".marker", $closest).hasClass("marker-ready")) { // marker-ready
+								render_map($('.acf-map', $closest));
+								$(".marker", $closest).addClass("marker-ready");
+							}
 						}
 					}).closest(".i-item").on("mouseleave.desktopPopupOpen", function() {
 						var _this = $(this);
@@ -784,8 +791,6 @@ var tmpl = {
 							$aArtist       = $("> a", $itemArtist),
 							sliderIcon, sliderIconTitle, sliderArtist;
 
-							console.log($aIconTitles);
-
 						$aIcon.add($aArtist).add($aIconTitles).off(".desktopPopupOpen").on("click.mobilePopupOpen", function(e) {
 							e.preventDefault();
 
@@ -842,7 +847,8 @@ var tmpl = {
 							$("#overlay").fadeIn();
 							$("#popup-artist").fadeIn();
 
-							var index = $imgArtist.index($("> img", this));
+							var index = $imgArtist.index($("> img", this)),
+								$elem;
 							
 							if(!$("#popup-artist").hasClass("slider-ready")) {
 								$("#popup-artist").addClass("slider-ready");
@@ -851,10 +857,31 @@ var tmpl = {
 									pager: false,
 									controls: false,
 									slideWidth: 230,
-									startSlide: index
+									startSlide: index,
+									
+									onSlideBefore: function($slideElement, oldIndex, newIndex) {
+										if($(".marker", $slideElement).length && !$(".marker", $slideElement).hasClass("marker-ready")) {
+											render_map($('.acf-map', $slideElement));
+											$(".marker", $slideElement).addClass("marker-ready");
+										}
+									}
 								});
+								
+								$elem = $("#popup-artist .popup-gallery__slider").children(":not(.bx-clone)").eq(sliderArtist.getCurrentSlide());
+								
+								if($(".marker", $elem).length && !$(".marker", $elem).hasClass("marker-ready")) {
+									render_map($('.acf-map', $elem));
+									$(".marker", $elem).addClass("marker-ready");
+								}
 							} else {
 								sliderArtist.goToSlide(index);
+								
+								$elem = $("#popup-artist .popup-gallery__slider").children(":not(.bx-clone)").eq(sliderArtist.getCurrentSlide());
+								
+								if($(".marker", $elem).length && !$(".marker", $elem).hasClass("marker-ready")) {
+									render_map($('.acf-map', $elem));
+									$(".marker", $elem).addClass("marker-ready");
+								}
 							}
 						});
 						
@@ -880,4 +907,158 @@ var tmpl = {
 			}());
 		});
 	});
+	
+	    /*
+     *  render_map
+     *
+     *  This function will render a Google Map onto the selected jQuery element
+     *
+     *  @type    function
+     *  @date    8/11/2013
+     *  @since    4.3.0
+     *
+     *  @param    $el (jQuery element)
+     *  @return    n/a
+     */
+
+    function render_map( $el ) {
+
+        // var
+        var $markers = $el.find('.marker');
+
+        // vars
+        var args = {
+            zoom        : 16,
+            center        : new google.maps.LatLng(0, 0),
+            mapTypeId    : google.maps.MapTypeId.ROADMAP
+        };
+
+        // create map
+        var map = new google.maps.Map( $el[0], args);
+
+        // add a markers reference
+        map.markers = [];
+
+        // add markers
+        $markers.each(function(){
+
+            add_marker( $(this), map );
+
+        });
+
+        // center map
+        center_map( map );
+
+    }
+
+    /*
+     *  add_marker
+     *
+     *  This function will add a marker to the selected Google Map
+     *
+     *  @type    function
+     *  @date    8/11/2013
+     *  @since    4.3.0
+     *
+     *  @param    $marker (jQuery element)
+     *  @param    map (Google Map object)
+     *  @return    n/a
+     */
+
+    function add_marker( $marker, map ) {
+
+        // var
+        var latlng = new google.maps.LatLng( $marker.attr('data-lat'), $marker.attr('data-lng') );
+
+        // create marker
+        var marker = new google.maps.Marker({
+            position    : latlng,
+            map            : map
+        });
+
+        // add to array
+        map.markers.push( marker );
+
+        // if marker contains HTML, add it to an infoWindow
+        if( $marker.html() )
+        {
+            // create info window
+            var infowindow = new google.maps.InfoWindow({
+                content        : $marker.html()
+            });
+
+            // show info window when marker is clicked
+            google.maps.event.addListener(marker, 'click', function() {
+
+                infowindow.open( map, marker );
+
+            });
+        }
+
+    }
+
+    /*
+     *  center_map
+     *
+     *  This function will center the map, showing all markers attached to this map
+     *
+     *  @type    function
+     *  @date    8/11/2013
+     *  @since    4.3.0
+     *
+     *  @param    map (Google Map object)
+     *  @return    n/a
+     */
+
+    function center_map( map ) {
+
+        // vars
+        var bounds = new google.maps.LatLngBounds();
+
+        // loop through all markers and create bounds
+        $.each( map.markers, function( i, marker ){
+
+            var latlng = new google.maps.LatLng( marker.position.lat(), marker.position.lng() );
+
+            bounds.extend( latlng );
+
+        });
+
+        // only 1 marker?
+        if( map.markers.length == 1 )
+        {
+            // set center of map
+            map.setCenter( bounds.getCenter() );
+            map.setZoom( 16 );
+        }
+        else
+        {
+            // fit to bounds
+            map.fitBounds( bounds );
+        }
+
+    }
+
+    /*
+     *  document ready
+     *
+     *  This function will render each map when the document is ready (page has loaded)
+     *
+     *  @type    function
+     *  @date    8/11/2013
+     *  @since    5.0.0
+     *
+     *  @param    n/a
+     *  @return    n/a
+     */
+
+    $(document).ready(function(){
+        if($('.acf-map').length) {
+            $('.acf-map').each(function(){
+
+                render_map( $(this) );
+
+            });
+        }
+    });
 }(jQuery));
